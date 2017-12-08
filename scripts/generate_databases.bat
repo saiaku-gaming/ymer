d43704bc1e43e9d0406b268bb04f1ac70274bd76
Mikael Berglund <mikbe@kth.se>
2017-12-08 22:38:11 +0100

    Added new databases

--- scripts/generate_databases.bat
+++ scripts/generate_databases.bat
@@ -8,9 +8,13 @@ dropdb --if-exists -U postgres character_service
 dropdb --if-exists -U postgres party_service
 dropdb --if-exists -U postgres friend_service
 dropdb --if-exists -U postgres wardrobe_service
+dropdb --if-exists -U postgres instace_service
+dropdb --if-exists -U postgres instace_container_service
 
 createdb -U postgres -O valhalla person_service
 createdb -U postgres -O valhalla character_service
 createdb -U postgres -O valhalla party_service
 createdb -U postgres -O valhalla friend_service
 createdb -U postgres -O valhalla wardrobe_service
+createdb -U postgres -O valhalla instance_service
+createdb -U postgres -O valhalla instance_container_service
