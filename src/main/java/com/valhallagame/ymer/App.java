c242b77e4e77964c87d05e23a2b55b555401117c
Mio Engel <mioengel@gmail.com>
2017-12-16 15:28:32 +0100

    added heartbeat to filter

--- src/main/java/com/valhallagame/ymer/App.java
+++ src/main/java/com/valhallagame/ymer/App.java
@@ -22,7 +22,7 @@ public class App {
 		FilterRegistrationBean registration = new FilterRegistrationBean();
 		registration.setFilter(getPersonAuthenticationFilter());
 		registration.addUrlPatterns("/v1/friend/*", "/v1/party/*", "/v1/character/*", "/v1/person/logout",
-				"/v1/utils/user-data", "/v1/wardrobe/*", "/v1/instance/*");
+				"/v1/person/heartbeat", "/v1/utils/user-data", "/v1/wardrobe/*", "/v1/instance/*");
 		registration.setName("personAuthenticationFilter");
 		registration.setOrder(1);
 		return registration;
