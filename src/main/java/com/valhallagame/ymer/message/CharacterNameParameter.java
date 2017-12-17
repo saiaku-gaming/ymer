30ce0b0c8db0492ac43cbdcda610e2c645744b80
Mikael Berglund <mikbe@kth.se>
2017-12-17 17:50:50 +0100

    party character things

--- src/main/java/com/valhallagame/ymer/message/CharacterNameParameter.java
+++ src/main/java/com/valhallagame/ymer/message/CharacterNameParameter.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CharacterParameter {
+public class CharacterNameParameter {
 	private String characterName;
 }
