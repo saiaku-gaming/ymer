50d4eb5b26e1e230a68d4fe83ba094c5be266ec7
Mikael Berglund <mikbe@kth.se>
2018-01-03 09:37:36 +0100

    Removed logback localy, logback file now exists on live server.

--- src/main/java/com/valhallagame/ymer/App.java
+++ src/main/java/com/valhallagame/ymer/App.java
@@ -1,7 +1,13 @@
 package com.valhallagame.ymer;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.servlet.Filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -12,8 +18,19 @@ import com.valhallagame.ymer.security.ServerAuthenticationFilter;
 
 @SpringBootApplication
 public class App {
+	
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
+		if (args.length > 0) {
+			// override system properties with local properties
+			try (InputStream inputStream = new FileInputStream(args[0])) {
+				System.getProperties().load(inputStream);
+			} catch (IOException e) {
+				logger.error("Failed to read input.", e);
+			}
+		}
+
 		SpringApplication.run(App.class, args);
 	}
 
