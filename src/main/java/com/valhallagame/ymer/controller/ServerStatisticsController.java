9c6cdf1b7a12406f4063a808d52ced787d620f43
Mikael Berglund <mikbe@kth.se>
2018-01-14 22:04:11 +0100

    Feat and statistics fixes

--- src/main/java/com/valhallagame/ymer/controller/ServerStatisticsController.java
+++ src/main/java/com/valhallagame/ymer/controller/ServerStatisticsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.valhallagame.common.JS;
 import com.valhallagame.statisticsserviceclient.StatisticsServiceClient;
-import com.valhallagame.statisticsserviceclient.message.IncrementParameter;
+import com.valhallagame.statisticsserviceclient.message.IncrementIntCounterParameter;
 
 
 @Controller
@@ -22,9 +22,9 @@ public class ServerStatisticsController {
 	@Autowired
 	StatisticsServiceClient statisticsServiceClient;
 
-	@RequestMapping(path = "/increment", method = RequestMethod.POST)
-	public ResponseEntity<JsonNode> getFriendData(@RequestBody IncrementParameter input) throws IOException {
-		return JS.message(statisticsServiceClient.increment(input.getCharacterName(), input.getKey(), input.getValue()));
+	@RequestMapping(path = "/increment-int-counter", method = RequestMethod.POST)
+	public ResponseEntity<JsonNode> getFriendData(@RequestBody IncrementIntCounterParameter input) throws IOException {
+		return JS.message(statisticsServiceClient.incrementIntCounter(input.getCharacterName(), input.getKey(), input.getValue()));
 	}
 
 }
