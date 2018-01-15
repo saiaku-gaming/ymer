2b53b508bfb5a10a576ea4e5b56bbed67c2e8c54
Mikael Berglund <mikbe@kth.se>
2018-01-15 21:35:01 +0100

    removed debug path

--- src/main/java/com/valhallagame/ymer/controller/WardrobeController.java
+++ src/main/java/com/valhallagame/ymer/controller/WardrobeController.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.valhallagame.common.JS;
 import com.valhallagame.wardrobeserviceclient.WardrobeServiceClient;
-import com.valhallagame.ymer.message.WardrobeItemParameter;
 
 @Controller
 @RequestMapping("/v1/wardrobe")
@@ -25,11 +23,5 @@ public class WardrobeController {
 	@RequestMapping(path = "/get-wardrobe-items", method = RequestMethod.GET)
 	public ResponseEntity<JsonNode> getWardrobeItems(@RequestAttribute("username") String username) throws IOException {
 		return JS.message(wardrobeServiceClient.getWardrobeItems(username));
-	}
-
-	@RequestMapping(path = "/debug-add-wardrobe-item", method = RequestMethod.POST)
-	public ResponseEntity<JsonNode> addWardrobeItem(@RequestAttribute("username") String username,
-			@RequestBody WardrobeItemParameter input) throws IOException {
-		return JS.message(wardrobeServiceClient.debugAddWardrobeItem(username, input.getItemName()));
 	}
 }
