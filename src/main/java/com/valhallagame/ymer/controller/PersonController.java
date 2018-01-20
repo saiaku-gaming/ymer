9e992b7b10c48809223c0925d66b682b425547df
Mio Engel <mioengel@gmail.com>
2018-01-20 19:23:03 +0100

    Added lowercase constraint

--- src/main/java/com/valhallagame/ymer/controller/PersonController.java
+++ src/main/java/com/valhallagame/ymer/controller/PersonController.java
@@ -3,6 +3,8 @@ package com.valhallagame.ymer.controller;
 import java.io.IOException;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,7 +41,7 @@ public class PersonController {
 
 	@RequestMapping(path = "/signup", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<JsonNode> signup(@RequestBody UsernamePasswordParameter input) throws IOException {
+	public ResponseEntity<JsonNode> signup(@Valid @RequestBody UsernamePasswordParameter input) throws IOException {
 		return JS.message(personServiceClient.signup(input.getUsername(), input.getPassword()));
 	}
 
--- src/main/java/com/valhallagame/ymer/message/WhisperCharacterParameter.java
+++ src/main/java/com/valhallagame/ymer/message/WhisperCharacterParameter.java
@@ -1,5 +1,7 @@
 package com.valhallagame.ymer.message;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class WhisperCharacterParameter {
+	@NotNull
 	private String message;
+	@NotNull
 	private String targetDisplayCharacterName;
 }
