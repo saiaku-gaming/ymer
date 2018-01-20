bdb9d3f52ea574f895f0991ff48944494ac81ef1
Mio Engel <mioengel@gmail.com>
2018-01-21 00:24:00 +0100

    Added more feat

--- src/main/java/com/valhallagame/ymer/controller/ServerStatisticsController.java
+++ src/main/java/com/valhallagame/ymer/controller/ServerStatisticsController.java
@@ -13,7 +13,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.valhallagame.common.JS;
 import com.valhallagame.statisticsserviceclient.StatisticsServiceClient;
 import com.valhallagame.statisticsserviceclient.message.IncrementIntCounterParameter;
-
+import com.valhallagame.statisticsserviceclient.message.UpdateHighTimerParameter;
+import com.valhallagame.statisticsserviceclient.message.UpdateLowTimerParameter;
 
 @Controller
 @RequestMapping("/v1/server-statistics")
@@ -23,8 +24,22 @@ public class ServerStatisticsController {
 	StatisticsServiceClient statisticsServiceClient;
 
 	@RequestMapping(path = "/increment-int-counter", method = RequestMethod.POST)
-	public ResponseEntity<JsonNode> getFriendData(@RequestBody IncrementIntCounterParameter input) throws IOException {
-		return JS.message(statisticsServiceClient.incrementIntCounter(input.getCharacterName(), input.getKey(), input.getValue()));
+	public ResponseEntity<JsonNode> incrementIntCounter(@RequestBody IncrementIntCounterParameter input)
+			throws IOException {
+		return JS.message(statisticsServiceClient.incrementIntCounter(input.getCharacterName(), input.getKey(),
+				input.getValue()));
+	}
+
+	@RequestMapping(path = "/update-low-timer", method = RequestMethod.POST)
+	public ResponseEntity<JsonNode> updateLowTimer(@RequestBody UpdateLowTimerParameter input) throws IOException {
+		return JS.message(statisticsServiceClient.updateLowTimer(input.getCharacterName(), input.getKey(),
+				input.getTimer()));
+	}
+
+	@RequestMapping(path = "/update-high-timer", method = RequestMethod.POST)
+	public ResponseEntity<JsonNode> updateHighTimer(@RequestBody UpdateHighTimerParameter input) throws IOException {
+		return JS.message(statisticsServiceClient.updateHighTimer(input.getCharacterName(), input.getKey(),
+				input.getTimer()));
 	}
 
 }
