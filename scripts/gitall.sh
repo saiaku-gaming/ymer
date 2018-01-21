b0c5d436cccacd7378ce68b3c3d758f5e5022f2d
Mio Engel <mioengel@gmail.com>
2018-01-21 13:02:45 +0100

    added git scropt

---
+++ scripts/gitall.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+PWD=$(pwd)
+
+if [ ! -e backend ]; then
+	mkdir backend
+fi
+cd backend
+
+for i in "feat" "character" "person" "statistics" "wardrobe" "notification" "friend" "chat" "party" "instance" "instance-container"; do
+	if [ ! -e $i-service-server ]; then
+		git clone "git@github.com:saiaku-gaming/$i-service-server.git"
+	fi
+	if [ ! -e $i-service-client ]; then
+		git clone "git@github.com:saiaku-gaming/$i-service-client.git"
+	fi
+done
+
+if [ ! -e ymer ]; then
+	git clone git@github.com:saiaku-gaming/ymer.git
+fi
+
+if [ ! -e valhalla-common ]; then
+	git clone git@github.com:saiaku-gaming/valhalla-common.git
+fi
+
+cd ..
+if [ ! -e misc ]; then
+	mkdir misc
+fi
+cd misc
+
+if [ ! -e jenkins-common ]; then
+	git clone git@github.com:saiaku-gaming/jenkins-common.git
+fi
+
+if [ ! -e valhalla-game.se ]; then
+	git clone git@github.com:saiaku-gaming/valhalla-game.se.git
+fi
+
