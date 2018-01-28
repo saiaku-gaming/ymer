6d5d3562b7e7c465b8d7a1d5106772aacb5ca3f1
Mikael Berglund <mikbe@kth.se>
2018-01-28 15:20:31 +0100

    TraitData fix

--- src/main/java/com/valhallagame/ymer/controller/TraitController.java
+++ src/main/java/com/valhallagame/ymer/controller/TraitController.java
@@ -1,13 +1,8 @@
 package com.valhallagame.ymer.controller;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestAttribute;
@@ -17,9 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.valhallagame.common.JS;
-import com.valhallagame.common.RestResponse;
 import com.valhallagame.traitserviceclient.TraitServiceClient;
-import com.valhallagame.traitserviceclient.message.TraitData;
 import com.valhallagame.traitserviceclient.message.SaveTraitBarIndexParameter;
 
 @Controller
@@ -31,19 +24,7 @@ public class TraitController {
 
 	@RequestMapping(path = "/get-traits", method = RequestMethod.GET)
 	public ResponseEntity<JsonNode> getTraits(@RequestAttribute("username") String username) throws IOException {
-
-		RestResponse<List<TraitData>> traitsResp = traitServiceClient.getTraits(username);
-
-		if (traitsResp.isOk()) {
-			Optional<List<TraitData>> opt = traitsResp.get();
-			if (opt.isPresent()) {
-				List<TraitData> list = opt.get();
-				Map<String, List<TraitData>> out = new HashMap<>();
-				out.put("traits", list);
-				return JS.message(HttpStatus.OK, out);
-			}
-		}
-		return JS.message(traitsResp);
+		return JS.message(traitServiceClient.getTraits(username));
 	}
 
 	@RequestMapping(path = "/save-trait-bar-index", method = RequestMethod.POST)
