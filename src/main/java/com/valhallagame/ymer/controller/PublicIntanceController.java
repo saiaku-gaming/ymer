dccaa579d6d64eef2eff0be9fb5fd5393191542f
Mio Engel <mioengel@gmail.com>
2018-06-02 17:10:46 +0200

    Added public fleet check

---
+++ src/main/java/com/valhallagame/ymer/controller/PublicIntanceController.java
@@ -0,0 +1,28 @@
+package com.valhallagame.ymer.controller;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.valhallagame.common.JS;
+import com.valhallagame.instanceserviceclient.InstanceServiceClient;
+
+@Controller
+@RequestMapping(path = "/v1/public-instance")
+public class PublicIntanceController {
+	
+	@Autowired
+	private InstanceServiceClient instanceServiceClient;
+	
+	@RequestMapping(path = "/get-fleets", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<JsonNode> getFleats() throws IOException {
+		return JS.message(instanceServiceClient.getFleets());
+	}
+}
