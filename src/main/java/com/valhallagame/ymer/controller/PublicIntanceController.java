f4cccd273334c399e2e1151c038508ec84a0893d
Mio Engel <mioengel@gmail.com>
2018-06-02 18:22:10 +0200

    Added cross origin to get fleets

--- src/main/java/com/valhallagame/ymer/controller/PublicIntanceController.java
+++ src/main/java/com/valhallagame/ymer/controller/PublicIntanceController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,6 +24,7 @@ public class PublicIntanceController {
 	private InstanceServiceClient instanceServiceClient;
 	
 	@RequestMapping(path = "/get-fleets", method = RequestMethod.GET)
+	@CrossOrigin
 	@ResponseBody
 	public ResponseEntity<JsonNode> getFleets() throws IOException {
 		return JS.message(HttpStatus.OK, instanceServiceClient.getFleets().get().orElse(new ArrayList<>()));
