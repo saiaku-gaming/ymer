ba363c87ec01f4515b0cf508a7982b7af4695e63
Mikael Berglund <mikbe@kth.se>
2018-07-15 00:34:16 +0200

    For some reason, sometimes Ymer can't talk to Notification service. Added a ping so that the healthcheck can notice it.
    Maybe we should add all services?

--- src/main/java/com/valhallagame/ymer/controller/RootController.java
+++ src/main/java/com/valhallagame/ymer/controller/RootController.java
@@ -1,5 +1,7 @@
 package com.valhallagame.ymer.controller;
 
+import com.valhallagame.notificationserviceclient.NotificationServiceClient;
+import com.valhallagame.traitserviceclient.TraitServiceClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,10 +11,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.valhallagame.common.JS;
 
+import java.io.IOException;
+
 @Controller
 public class RootController {
 	@RequestMapping(path = "/", method = RequestMethod.GET)
-	public ResponseEntity<JsonNode> ping() {
+	public ResponseEntity<JsonNode> ping() throws IOException {
+		// For some reason, sometimes Ymer can't talk to Notification service. Added a ping so that the
+		// healthcheck can notice it. Maybe we should add all services?
+		NotificationServiceClient.get().ping();
 		return JS.message(HttpStatus.OK, "pong");
 	}
 }
