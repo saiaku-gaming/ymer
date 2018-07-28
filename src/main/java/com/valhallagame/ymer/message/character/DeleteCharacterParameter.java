5596792bd85b4b40f84bfcc0dd7e043f9b1b0f9e
Mikael Berglund <mikbe@kth.se>
2018-07-28 03:02:40 +0200

    removed @CheckLowercase on displayCharacterName

--- src/main/java/com/valhallagame/ymer/message/character/DeleteCharacterParameter.java
+++ src/main/java/com/valhallagame/ymer/message/character/DeleteCharacterParameter.java
@@ -1,6 +1,5 @@
 package com.valhallagame.ymer.message.character;
 
-import com.valhallagame.common.validation.CheckLowercase;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +10,5 @@ import org.hibernate.validator.constraints.NotBlank;
 @AllArgsConstructor
 public final class DeleteCharacterParameter {
   @NotBlank
-  @CheckLowercase
   String displayCharacterName;
 }
