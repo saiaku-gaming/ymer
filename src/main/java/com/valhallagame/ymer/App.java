3ee5b074a67234c590bd023164173fbbd1ac69c7
Mikael Berglund <mikbe@kth.se>
2018-10-09 00:29:48 +0200

    Fixed server auth

--- src/main/java/com/valhallagame/ymer/App.java
+++ src/main/java/com/valhallagame/ymer/App.java
@@ -26,9 +26,22 @@ public class App {
 	public FilterRegistrationBean personAuthenticationFilterRegistration() {
 		FilterRegistrationBean registration = new FilterRegistrationBean();
 		registration.setFilter(getPersonAuthenticationFilter());
-		registration.addUrlPatterns("/v1/friend/*", "/v1/party/*", "/v1/character/*", "/v1/person/logout",
-				"/v1/person/heartbeat", "/v1/utils/user-data", "/v1/wardrobe/*", "/v1/instance/*", "/v1/chat/*",
-				"/v1/feat/*", "/v1/trait/*", "/v1/actionbar/*", "/v1/currency/*");
+        registration.addUrlPatterns(
+                "/v1/friend/*",
+                "/v1/party/*",
+                "/v1/character/*",
+                "/v1/person/logout",
+                "/v1/person/heartbeat",
+                "/v1/utils/user-data",
+                "/v1/wardrobe/*",
+                "/v1/instance/*",
+                "/v1/chat/*",
+                "/v1/feat/*",
+                "/v1/trait/*",
+                "/v1/actionbar/*",
+                "/v1/currency/*",
+                "/v1/recipe/*"
+        );
 		registration.setName("personAuthenticationFilter");
 		registration.setOrder(1);
 		return registration;
@@ -38,8 +51,16 @@ public class App {
 	public FilterRegistrationBean serverAuthenticationFilterRegistration() {
 		FilterRegistrationBean registration = new FilterRegistrationBean();
 		registration.setFilter(getServerAuthenticationFilter());
-		registration.addUrlPatterns("/v1/server-recipe/*, /v1/server-wardrobe/*, /v1/server-character/*", "/v1/server-instance/*",
-				"/v1/server-feat/*", "/v1/server-statistics/*", "/v1/server-trait/*", "/v1/server-currency/*");
+        registration.addUrlPatterns(
+                "/v1/server-recipe/*",
+                "/v1/server-wardrobe/*",
+                "/v1/server-character/*",
+                "/v1/server-instance/*",
+                "/v1/server-feat/*",
+                "/v1/server-statistics/*",
+                "/v1/server-trait/*",
+                "/v1/server-currency/*"
+        );
 		registration.setName("serverAuthenticationFilter");
 		registration.setOrder(1);
 		return registration;
